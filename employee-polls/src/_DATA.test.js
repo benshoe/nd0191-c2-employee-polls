14667e936efb9c37310f41190afe89529ee62735
Ben Schoen <benshoe@gmail.com>
2023-11-22 17:05:32 +0100

    test: unit tests created for saveQuestionAnswer

--- employee-polls/src/_DATA.test.js
+++ employee-polls/src/_DATA.test.js
@@ -1,25 +1,59 @@
-import {_saveQuestion} from "./_DATA";
+import {_saveQuestion, _saveQuestionAnswer} from "./_DATA";
 
 describe('DATA', () => {
+    describe('saveQuestion', () => {
 
-    it('saveQuestion returns correct data', async () => {
-        const result = await _saveQuestion({author: 'mtsamis', optionOneText: 'Be happy', optionTwoText: 'Be rich'});
-        expect(result.author).toEqual('mtsamis');
-        expect(result.optionOne.text).toEqual('Be happy');
-        expect(result.optionOne.votes).toEqual([]);
-        expect(result.optionTwo.text).toEqual('Be rich');
-        expect(result.optionTwo.votes).toEqual([]);
-        expect(result.id).toBeDefined();
-        expect(result.timestamp).toBeDefined();
+
+        it('returns correct data', async () => {
+            const result = await _saveQuestion({
+                author: 'mtsamis',
+                optionOneText: 'Be happy',
+                optionTwoText: 'Be rich'
+            });
+            expect(result.author).toEqual('mtsamis');
+            expect(result.optionOne.text).toEqual('Be happy');
+            expect(result.optionOne.votes).toEqual([]);
+            expect(result.optionTwo.text).toEqual('Be rich');
+            expect(result.optionTwo.votes).toEqual([]);
+            expect(result.id).toBeDefined();
+            expect(result.timestamp).toBeDefined();
+        });
+
+        it('returns error', async () => {
+            await expect(_saveQuestion({})).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+            await expect(_saveQuestion({author: 'mtsamis'})).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+            await expect(_saveQuestion({optionOneText: 'Be happy'})).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+            await expect(_saveQuestion({optionTwoText: 'Be rich'})).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+            await expect(_saveQuestion({
+                author: 'mtsamis',
+                optionOneText: 'Be happy'
+            })).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+            await expect(_saveQuestion({
+                author: 'mtsamis',
+                optionOneTwo: 'Be rich'
+            })).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+            await expect(_saveQuestion({
+                optionOneText: 'Be happy',
+                optionOneTwo: 'Be rich'
+            })).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+        });
     });
 
-    it('saveQuestion returns error', async () => {
-        await expect(_saveQuestion({})).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
-        await expect(_saveQuestion({author: 'mtsamis'})).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
-        await expect(_saveQuestion({optionOneText: 'Be happy'})).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
-        await expect(_saveQuestion({optionTwoText: 'Be rich'})).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
-        await expect(_saveQuestion({author: 'mtsamis', optionOneText: 'Be happy'})).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
-        await expect(_saveQuestion({author: 'mtsamis', optionOneTwo: 'Be rich'})).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
-        await expect(_saveQuestion({optionOneText: 'Be happy', optionOneTwo: 'Be rich'})).rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+    describe('saveQuestionAnswer', () => {
+
+        it('returns correct data', async () => {
+            const result = await _saveQuestionAnswer({
+                authedUser: 'mtsamis',
+                qid: 'loxhs1bqm25b708cmbf3g',
+                answer: 'optionOne'
+            });
+
+            expect(result).toBeTruthy();
+        });
+
+        it('throws error', async () => {
+            await expect(_saveQuestionAnswer({})).rejects.toEqual('Please provide authedUser, qid, and answer')
+        })
     });
+
 })
