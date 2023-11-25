a6c19a1fa5d3daebd30ba1d4cc4e0455bf2a06f5
Ben Schoen <benshoe@gmail.com>
2023-11-25 15:30:15 +0100

    feature: avatars added

--- employee-polls/src/App.js
+++ employee-polls/src/App.js
@@ -1,11 +1,49 @@
 import './App.css';
+import {useEffect, useState} from "react";
+import {_getUsers} from "./_DATA";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Employee Polls</h1>
-    </div>
-  );
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const getUsers = async () => {
+            console.log('getUsers wordt aangeroepen')
+            const resp = await _getUsers();
+            let entries = Object.entries(resp);
+            // console.log(entries);
+            // console.log(resp);
+            let from = Array.from(entries);
+            // console.log('----');
+            // console.log(from);
+            console.log(`Keys: ${Object.keys(resp)}`);
+            console.log(`Values: ${JSON.stringify(Object.values(resp))}`);
+            console.log(`Entries: ${JSON.stringify(Object.entries(resp))}`);
+
+            setUsers(resp);
+        };
+
+        getUsers();
+    }, []);
+
+    const myStyle = {
+        color: "blue",
+        listStyleType: "none"
+    }
+
+    return (
+        <div className="App">
+            <h1>Employee Polls</h1>
+            <ol style={myStyle}>
+                { Object.keys(users).map((key) =>
+                    <li style={{marginBottom: "50px"}} key={users[key].id}>
+                        <img width={150} src={users[key].avatarURL} alt={users[key].avatarURL} />
+                        <div>{users[key].name}</div>
+                    </li>)
+                }
+            </ol>
+        </div>
+    );
 }
 
 export default App;
