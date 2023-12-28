0d1635bb25387a20ac4fcbfa67032ef0b590c289
Ben Schoen <benshoe@gmail.com>
2023-12-28 15:49:57 +0100

    Feature: toggle between answered and unanswered questions

--- employee-polls/src/components/Home.js
+++ employee-polls/src/components/Home.js
@@ -2,22 +2,31 @@ import {connect} from "react-redux";
 import {handleInitialData} from "../actions/shared";
 import PropTypes from "prop-types";
 import Polls from "./Polls";
+import {useState} from "react";
 
 const Home = () => {
 
     handleInitialData();
 
+    const [answeredPolls, setAnsweredPolls] = useState(false);
+
+    const answered = (e) => {
+        const answeredPolls = e.target.value === 'Answered';
+        setAnsweredPolls(answeredPolls);
+    };
+
     return (
         <div>
-            <div>
+            <form>
                 <label>
-                    <input type="radio" name="polls" id="unanswered" value="Unanswered" checked/>Unanswered
+                        <input type="radio" name="polls" id="unanswered" value="Unanswered" onChange={answered} defaultChecked />Unanswered
                 </label>
+
                 <label>
-                    <input type="radio" name="polls" id="answered" value="Answered"/>Answered
+                    <input type="radio" name="polls" id="answered" value="Answered" onChange={answered}/>Answered
                 </label>
-            </div>
-            <Polls/>
+            </form>
+            <Polls answered={answeredPolls}/>
         </div>
     )
 }
--- employee-polls/src/components/Polls.js
+++ employee-polls/src/components/Polls.js
@@ -9,7 +9,7 @@ const Polls = (props) => {
             {
                 props.questionIds.map((id) => (
                     <li key={id}>
-                        <Poll id={id} />
+                        <Poll id={id}/>
                     </li>
                 ))
             }
@@ -17,10 +17,14 @@ const Polls = (props) => {
     </div>
 }
 
-const mapStateToProps = ({questions}) => ({
-    questionIds: Object.keys(questions).sort(
-        (a, b) => questions[b].timestamp - questions[a].timestamp
-    )
-});
+const mapStateToProps = ({authedUser, questions, users}, {answered}) => {
+    const answeredIds = Object.keys(users[authedUser].answers);
+
+    const questionIds = Object.keys(questions).filter(id => answered ? answeredIds.includes(id) : !answeredIds.includes(id)).sort(
+        (a, b) => questions[b].timestamp - questions[a].timestamp);
+    return {
+        questionIds
+    }
+};
 
 export default connect(mapStateToProps)(Polls);
