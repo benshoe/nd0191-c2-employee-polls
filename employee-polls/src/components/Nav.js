75143b6aa03c0c9e54d9b6e0f87a608e70d81d39
Ben Schoen <benshoe@gmail.com>
2024-01-17 15:28:21 +0100

    Styling: styling for Nav bar adjusted.

--- employee-polls/src/components/Nav.js
+++ employee-polls/src/components/Nav.js
@@ -6,11 +6,11 @@ const Nav = (props) => {
     return (
         <nav className="nav">
             <ul className="menu">
-                <li key="home"><Link to="/">Home</Link></li>
+                <li className="first-item" key="home"><Link to="/">Home</Link></li>
                 {props.user ? <li key="leaderboard"><Link to="/leaderboard">Leaderboard</Link></li> : null }
                 {props.user ? <li key="newpoll"><Link className="link-button" to="/add">New poll</Link></li> : null }
                 <h1 className="nav-title">Employee polls</h1>
-                {props.user ? <li key="logout"><Link to="/logout">Logout {props.user}</Link></li> : null}
+                {props.user ? <li className="last-item" key="logout"><Link to="/logout">Logout {props.user}</Link></li> : null}
             </ul>
         </nav>
     )
