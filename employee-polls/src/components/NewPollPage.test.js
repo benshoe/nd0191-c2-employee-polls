3d7140b6cd80858c3c067bd0efc70eeb225b39a4
Ben Schoen <benshoe@gmail.com>
2024-01-17 14:57:30 +0100

    Refactor: creating and mocking the store removed from test as it is no longer needed.

--- employee-polls/src/components/NewPollPage.test.js
+++ employee-polls/src/components/NewPollPage.test.js
@@ -1,20 +1,61 @@
 import NewPollPage from "./NewPollPage";
-import thunk from "redux-thunk";
-import configureStore from "redux-mock-store";
 import {renderWithProviders} from "../utils/test-utils";
-import logger from "../middleware/logger";
+import userEvent from "@testing-library/user-event";
 
-const middlewares = [thunk, logger];
-const mockStore = configureStore(middlewares);
-const store = mockStore();
 describe('Adding new poll', () => {
-    it('should have disabled Submit-button', async () => {
-        var component = renderWithProviders(<NewPollPage />, {
+    function createComponent() {
+        return renderWithProviders(<NewPollPage/>, {
             preloadedState: {
                 authedUser: 'tylermcginnis'
             }
         });
-        const submitButton = await component.getByText('Submit');
+    }
+
+    async function getSubmitButton(component) {
+        return component.getByText('Submit');
+    }
+
+    it('should have disabled Submit-button', async () => {
+        const component = createComponent();
+
+        const submitButton = await getSubmitButton(component);
+        expect(submitButton).toBeDisabled();
+    });
+
+    it('should have enabled Submit-button when both input options are filled', async () => {
+        const component = createComponent();
+
+        const inputOption1 = await component.getByLabelText('Option 1:');
+        await userEvent.type(inputOption1, "Have a succeeding unit test");
+        expect(inputOption1.value).toBe("Have a succeeding unit test");
+
+        const inputOption2 = await component.getByLabelText('Option 2:');
+        await userEvent.type(inputOption2, "Have a useful unit test");
+        expect(inputOption2.value).toBe("Have a useful unit test");
+
+        const submitButton = await getSubmitButton(component);
+        expect(submitButton).toBeEnabled();
+    });
+
+    it('should have disabled Submit-button when only input option 1 is filled', async () => {
+        const component = createComponent();
+
+        const inputOption1 = await component.getByLabelText('Option 1:');
+        await userEvent.type(inputOption1, "Have a succeeding unit test");
+        expect(inputOption1.value).toBe("Have a succeeding unit test");
+
+        const submitButton = await getSubmitButton(component);
+        expect(submitButton).toBeDisabled();
+    });
+
+    it('should have disabled Submit-button when only input option 2 is filled', async () => {
+        const component = createComponent();
+
+        const inputOption2 = await component.getByLabelText('Option 2:');
+        await userEvent.type(inputOption2, "Have a useful unit test");
+        expect(inputOption2.value).toBe("Have a useful unit test");
+
+        const submitButton = await getSubmitButton(component);
         expect(submitButton).toBeDisabled();
     });
 });
